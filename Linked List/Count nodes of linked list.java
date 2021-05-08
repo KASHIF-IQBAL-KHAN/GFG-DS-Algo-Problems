ce01552782bdc02c3cc37789c9a47068674717c6
Kashif Iqbal Khan <kashifiqbalkhan30@gmail.com>
2021-05-08 23:46:39 +0530

    8 May

---
+++ Linked List/Count nodes of linked list.java
@@ -0,0 +1,54 @@
+/*
+Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function would be added by GFG's Online Judge.
+
+Given a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+Output: 5
+Explanation: Count of nodes in the 
+linked list is 5, which is its length.
+
+Example 2:
+
+Input:
+LinkedList: 2->4->6->7->5->1->0
+Output: 7
+Explanation: Count of nodes in the
+linked list is 7. Hence, the output
+is 7.
+Your Task:
+Your task is to complete the given function getCount(), which takes a head reference as an argument and should return the length of the linked list.
+
+Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(1)
+
+Constraints:
+1 <= N <= 105
+1 <= value <= 103
+*/
+
+//  Kashif Iqbal
+//  08-May-2021
+ 
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+    
+        int count = 0;
+        
+        while(head != NULL){
+            head = head -> next;
+            ++count;
+        }
+        
+        return count;
+    }
+};
+    
