45f3006c9b91d532180e6d5cc2489e4259d5f7b4
Kashif Iqbal Khan <kashifiqbalkhan30@gmail.com>
2021-05-16 14:50:41 +0530

    16 May

---
+++ String/Run Length Encoding.java
@@ -0,0 +1,60 @@
+/*
+Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function would be added by GFG's Online Judge.
+
+Given a string, Your task is to  complete the function encode that returns the run length encoded string for the given string.
+eg if the input string is “wwwwaaadexxxxxx”, then the function should return “w4a3d1e1x6″.
+You are required to complete the function encode that takes only one argument the string which is to be encoded and returns the encoded string.
+
+Example 1:
+
+Input:
+str = aaaabbbccc
+Output: a4b3c3
+Explanation: a repeated 4 times
+consecutively b 3 times, c also 3
+times.
+
+Example 2:
+
+Input:
+str = abbbcdddd
+Output: a1b3c1d4
+Your Task:
+Complete the function encode() which takes a character array as a input parameter and returns the encoded string.
+
+Expected Time Complexity: O(N), N = length of given string.
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1<=length of str<=100
+*/
+
+// Kashif Iqbal
+// 16-May-2021	
+
+
+class GfG
+ {
+	String encode(String str)
+	{
+
+        StringBuilder sb = new StringBuilder();
+          
+        for(int itr = 0; itr < str.length(); ++itr){
+
+            sb.append(str.charAt(itr));
+            int count = 1;
+              
+            while(itr < str.length()-1 && str.charAt(itr) == str.charAt(itr+1)){
+                ++count;
+                itr = itr + 1;
+            }
+              
+            sb.append(count);
+        }
+          
+        return sb.toString();
+	}
+ }
